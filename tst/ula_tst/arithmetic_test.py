c2f644d2c72350d208d71f0e86e2bf3e6695969b
Beatriz Rochelle <rochelle.beatriz@gmail.com>
2023-10-30 15:28:55 +0100

    chore: lint code, use explicit values in asserts

--- tst/ula_tst/arithmetic_test.py
+++ tst/ula_tst/arithmetic_test.py
@@ -29,7 +29,7 @@ async def and_with_zero(dut):
     dut.a.value = 0x0000_0000
 
     await RisingEdge(dut.clk)
-    assert dut.s.value == dut.a.value, f'And mismatch: s: {dut.s.value} != {dut.a.value}.'
+    assert dut.s.value == 0x0000_0000, f'And mismatch: s: {dut.s.value} != {dut.a.value}.'
 
 @cocotb.test(skip=False)
 async def and_with_maximum(dut):
@@ -61,7 +61,7 @@ async def or_with_maximum(dut):
     dut.a.value = 0xFFFF_FFFF
 
     await RisingEdge(dut.clk)
-    assert dut.s.value == dut.a.value, f'And mismatch: s: {dut.s.value} != {dut.a.value}.'
+    assert dut.s.value == 0xFFFF_FFFF, f'And mismatch: s: {dut.s.value} != {dut.a.value}.'
 
 ##############################################################
 
@@ -74,7 +74,7 @@ async def add_with_zero(dut):
         dut.a.value = 0
 
         await RisingEdge(dut.clk);
-        assert dut.s.value == dut.b.value, f'Adding mismatch: lo: {dut.s.value} != {dut.b.value}.'
+        assert dut.s.value == dut.b.value, f'Adding mismatch: s: {dut.s.value} != {dut.b.value}.'
 
 
 @cocotb.test(skip=False)
@@ -86,7 +86,7 @@ async def add_by_one(dut):
         dut.a.value = 1
 
         await RisingEdge(dut.clk);
-        assert dut.s.value == dut.b.value + 1, f'Adding mismatch: lo: {dut.s.value} != {dut.b.value + 1}.'
+        assert dut.s.value == dut.b.value + 1, f'Adding mismatch: s: {dut.s.value} != {dut.b.value + 1}.'
 
 
 @cocotb.test(skip=False)
@@ -97,7 +97,7 @@ async def add_by_maximum(dut):
     dut.b.value = 0xFFFF_FFFF;
    
     await RisingEdge(dut.clk);
-    assert dut.s.value == (dut.a.value + dut.b.value) & 0xFFFF_FFFF, f'Adding mismatch: lo: {hex(dut.s.value.integer)} != {hex((dut.a.value.integer + dut.b.value.integer) & 0xFFFF_FFFF)}.'
+    assert dut.s.value == (dut.a.value + dut.b.value) & 0xFFFF_FFFF, f'Adding mismatch: s: {hex(dut.s.value.integer)} != {hex((dut.a.value.integer + dut.b.value.integer) & 0xFFFF_FFFF)}.'
 
 
 @cocotb.test(skip=False)
@@ -108,7 +108,7 @@ async def add_by_n_times(dut):
         tb.randomize_input();
 
         await RisingEdge(dut.clk);
-        assert dut.s.value == (dut.a.value + dut.b.value) & 0xFFFF_FFFF, f'Adding mismatch: lo: {hex(dut.s.value.integer & 0xFFFF_FFFF)} != {hex((dut.a.value.integer + dut.b.value.integer) & 0xFFFF_FFFF)}.'
+        assert dut.s.value == (dut.a.value + dut.b.value) & 0xFFFF_FFFF, f'Adding mismatch: s: {hex(dut.s.value.integer & 0xFFFF_FFFF)} != {hex((dut.a.value.integer + dut.b.value.integer) & 0xFFFF_FFFF)}.'
 
 ###############################################################
 
@@ -123,7 +123,7 @@ async def sub_with_zero(dut):
         await RisingEdge(dut.clk);
         # Python uses int as 64 bits. We are representing it with only 32,
         # masking is needed when comparing negative numbers
-        assert dut.s.value == -dut.b.value & 0xffff_ffff, f'Subtracting mismatch: lo: {hex(dut.s.value.integer)} != {hex(-dut.b.value.integer & 0xffff_ffff)}.'
+        assert dut.s.value == -dut.b.value & 0xFFFF_FFFF, f'Subtracting mismatch: s: {hex(dut.s.value.integer)} != {hex(-dut.b.value.integer & 0xffff_ffff)}.'
 
 
 @cocotb.test(skip=False)
@@ -137,7 +137,7 @@ async def sub_one_less_number(dut):
         await RisingEdge(dut.clk);
         # Python uses int as 64 bits. We are representing it with only 32,
         # masking is needing when comparing negative numbers
-        assert dut.s.value == (1-dut.b.value) & 0xffff_ffff, f'Subtracting mismatch: lo: {hex(dut.s.value.integer)} != {hex((1-dut.b.value.integer) & 0xffff_ffff)}.'
+        assert dut.s.value == (1-dut.b.value) & 0xFFFF_FFFF, f'Subtracting mismatch: s: {hex(dut.s.value.integer)} != {hex((1-dut.b.value.integer) & 0xffff_ffff)}.'
 
 
 @cocotb.test(skip=False)
@@ -148,7 +148,7 @@ async def sub_by_maximum(dut):
     dut.b.value = 0xFFFF_FFFF
    
     await RisingEdge(dut.clk);
-    assert dut.s.value == (dut.a.value - dut.b.value) & 0xFFFF_FFFF, f'Subtracting mismatch: lo: {hex(dut.s.value.integer)} != {hex((dut.a.value.integer - dut.b.value.integer) & 0xFFFF_FFFF)}.'
+    assert dut.s.value == (dut.a.value - dut.b.value) & 0xFFFF_FFFF, f'Subtracting mismatch: s: {hex(dut.s.value.integer)} != {hex((dut.a.value.integer - dut.b.value.integer) & 0xFFFF_FFFF)}.'
 
 
 @cocotb.test(skip=False)
@@ -159,7 +159,7 @@ async def sub_by_n_times(dut):
         tb.randomize_input();
 
         await RisingEdge(dut.clk);
-        assert dut.s.value == (dut.a.value - dut.b.value) & 0xFFFF_FFFF, f'Adding mismatch: lo: {hex(dut.s.value.integer & 0xFFFF_FFFF)} != {hex((dut.a.value.integer - dut.b.value.integer) & 0xFFFF_FFFF)}.'
+        assert dut.s.value == (dut.a.value - dut.b.value) & 0xFFFF_FFFF, f'Adding mismatch: s: {hex(dut.s.value.integer & 0xFFFF_FFFF)} != {hex((dut.a.value.integer - dut.b.value.integer) & 0xFFFF_FFFF)}.'
 
 ###############################################################
 
@@ -172,9 +172,9 @@ async def slt_zero(dut):
 
     await RisingEdge(dut.clk)
     if dut.a.value < dut.b.value:   
-        assert dut.s.value == 0x1, f'And mismatch: lo: {dut.s.value} != 11111111.'
+        assert dut.s.value == 0x1, f'And mismatch: s: {dut.s.value} != 11111111.'
     else:
-        assert dut.s.value ==  0x0, f'And mismatch: lo: {dut.s.value} != 00000000.'
+        assert dut.s.value ==  0x0, f'And mismatch: s: {dut.s.value} != 00000000.'
 
 #rever
 @cocotb.test(skip=False)
@@ -186,6 +186,6 @@ async def slt_maximum(dut):
 
     await RisingEdge(dut.clk)
     if dut.a.value > dut.b.value:   
-        assert dut.s.value == 0x0000_0000, f'And mismatch: lo: {dut.s.value} != 0x0000_0000.'
+        assert dut.s.value == 0x0000_0000, f'And mismatch: s: {dut.s.value} != 0x0000_0000.'
     else:
-        assert dut.s.value ==  0xFFFF_FFFF, f'And mismatch: lo: {dut.s.value} != 0xFFFF_FFFF.'
+        assert dut.s.value ==  0xFFFF_FFFF, f'And mismatch: s: {dut.s.value} != 0xFFFF_FFFF.'
