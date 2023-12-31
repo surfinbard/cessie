da21c0dea6cc043df959834534600cfbedf86f62
Beatriz Rochelle <rochelle.beatriz@gmail.com>
2023-12-31 18:39:11 -0300

    feat: add test for instrmemo

---
+++ tst/instr_memo/instr_memo_test.py
@@ -0,0 +1,24 @@
+import cocotb
+from cocotb.triggers import Timer
+class TB:
+    dut = cocotb.top
+
+    def __init__(self):
+        for i in range(32):
+            TB.dut.registers[i] = 0
+
+    async def config(self):
+        for i in range(32):
+            TB.dut.registers[i] = i    
+            await Timer(10, units='ns')
+
+###############################################################
+    
+@cocotb.test(skip=False)
+async def instr_read(dut):
+    tb = TB()
+    await tb.config()
+   
+    for i in range(32):
+        assert dut.registers[i].value == i, f'Control mismatch in InstrMemo: {dut.registers[i].value} != {i}.'
+
